2c7d0949c065ec180418c78a3cf4581ee7945130
ohericaoh <oh.erica19@gmail.com>
2020-11-16 14:21:31 +0800

    Finished Challenge 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,28 @@
 class QuestionsController < ApplicationController
-  def ask
 
+  def home
+    @welcome
   end
+
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  private
+
+  def coach_answer(your_message)
+  if your_message.downcase == "i am going to work"
+    "Great!"
+  elsif your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
+  end
+
 end
